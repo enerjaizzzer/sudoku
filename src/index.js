d4986d32e7b2149d370f6b500967c28dabeb5499
enerjaizzzer <enerjaizzzer@gmail.com>
2019-03-15 17:05:46 +0300

    ololo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var stalth = [];
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      if (matrix[i][j] === 0) {
+        var row = i;
+        var arr = [];
+        matrix[row].forEach(function (number) {
+          if (number !== 0) {
+            arr.push(number);
+            stalth = numbers.filter(function (item, k) {
+              var ok = arr.indexOf(item) === -1;
+              if (ok) {
+                stalth.push(i + 1);
+              }
+              return ok;
+            }
+            )
+
+            console.log(stalth);
+          }
+
+        })
+
+      }
+    }
+  }
 }
